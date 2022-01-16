1a1225cb2543515080abebcb54f2f863a29cd235
Nilesh Vishwakarma <iamnilesh2020@gmail.com>
2022-01-16 18:39:42 +0530

    Add project files.

---
+++ FibonacchiSeriesPrint/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibonacchi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 1, c;
+            Console.Write("Enter a Number:");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            for (int i = 0; i < number; i++)
+            {
+                c = a + b;
+                Console.WriteLine(c);
+
+                a = b;
+                b = c;
+
+
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
